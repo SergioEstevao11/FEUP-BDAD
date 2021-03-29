6ff9e021a9961409da60bd8cc8c950d101d70396
rodrigotuna <rodrigotuna.andrade@gmail.com>
2021-03-29 20:06:57 +0100

    started povoar

---
+++ povoar.sql
@@ -0,0 +1,343 @@
+PRAGMA foreign_keys = on;
+
+--Table : Servico
+INSERT INTO Servico (   
+                        nome
+                    )
+                    VALUES(
+                        "Alfa-Pendular"
+                    );
+            
+INSERT INTO Servico (   
+                        nome
+                    )
+                    VALUES(
+                        "Intercidades"
+                    );
+
+INSERT INTO Servico (   
+                        nome
+                    )
+                    VALUES(
+                        "Regional"
+                    );
+
+INSERT INTO Servico (   
+                        nome
+                    )
+                    VALUES(
+                        "Urbano"
+                    );
+
+--Table : Cliente
+INSERT INTO Cliente (   
+                        nif,
+                        nome
+                    )
+                    VALUES(
+                        "131199282",
+                        "Rodrigo Tuna de Andrade"
+                    );
+
+INSERT INTO Cliente (   
+                        nif,
+                        nome
+                    )
+                    VALUES(
+                        "766573459",
+                        "Clara Gonçalves da Costa"
+                    );
+
+INSERT INTO Cliente (   
+                        nif,
+                        nome
+                    )
+                    VALUES(
+                        "834419570",
+                        "Joaquim Fernando Teixeira"
+                    );
+
+INSERT INTO Cliente (   
+                        nif,
+                        nome
+                    )
+                    VALUES(
+                        "545580961",
+                        "Alexandre Borges Lopes"
+                    );
+
+INSERT INTO Cliente (   
+                        nif,
+                        nome
+                    )
+                    VALUES(
+                        "773734252",
+                        "Sérgio Miguel Rosa Estêvão"
+                    );
+
+INSERT INTO Cliente (   
+                        nif,
+                        nome
+                    )
+                    VALUES(
+                        "777374837",
+                        "Rogério Antunes Figueiredo"
+                    );
+
+INSERT INTO Cliente (   
+                        nif,
+                        nome
+                    )
+                    VALUES(
+                        "156703269",
+                        "João Tiago Pereira Lopes"
+                    );
+
+INSERT INTO Cliente (   
+                        nif,
+                        nome
+                    )
+                    VALUES(
+                        "115299474",
+                        "Beatriz Teixeira da Cunha"
+                    );
+
+INSERT INTO Cliente (   
+                        nif,
+                        nome
+                    )
+                    VALUES(
+                        "881739114",
+                        "Jorge Marques Almeida"
+                    );
+
+INSERT INTO Cliente (   
+                        nif,
+                        nome
+                    )
+                    VALUES(
+                        "570208196",
+                        "Ricardo Jorge Pinto"
+                    );
+
+INSERT INTO Cliente (   
+                        nif,
+                        nome
+                    )
+                    VALUES(
+                        "459032296",
+                        "Elisa Nogueira Teixeira"
+                    );
+
+INSERT INTO Cliente (   
+                        nif,
+                        nome
+                    )
+                    VALUES(
+                        "099922403",
+                        "Miguel Monteiro Mendes"
+                    );
+
+INSERT INTO Cliente (   
+                        nif,
+                        nome
+                    )
+                    VALUES(
+                        "219213078",
+                        "Rafaela Alexandra Silva"
+                    );
+
+INSERT INTO Cliente (   
+                        nif,
+                        nome
+                    )
+                    VALUES(
+                        "025456613",
+                        "Fernanda Lima Raposo"
+                    );
+
+INSERT INTO Cliente (   
+                        nif,
+                        nome
+                    )
+                    VALUES(
+                        "201329033",
+                        "Margarida Gomes Godinho Completo"
+                    );
+
+INSERT INTO Cliente (   
+                        nif,
+                        nome
+                    )
+                    VALUES(
+                        "456702207",
+                        "Joana Ribeiro Gomes"
+                    );
+
+INSERT INTO Cliente (   
+                        nif,
+                        nome
+                    )
+                    VALUES(
+                        "798467573",
+                        "Susana Isabel Lessa"
+                    );
+
+INSERT INTO Cliente (   
+                        nif,
+                        nome
+                    )
+                    VALUES(
+                        "623677721",
+                        "Vasco Miguel Macedo"
+                    );
+
+INSERT INTO Cliente (   
+                        nif,
+                        nome
+                    )
+                    VALUES(
+                        "689860351",
+                        "Maria do Céu Ferreira"
+                    );
+
+INSERT INTO Cliente (   
+                        nif,
+                        nome
+                    )
+                    VALUES(
+                        "617323133",
+                        "Matilde Miranda Vaz"
+                    );
+
+
+--Table: Estacao
+INSERT INTO Estacao (   
+                        nome,
+                        morada,
+                        codigoPostal,
+                        localidade
+                    )
+                    VALUES(
+                        "Campanhã",
+                        "Largo da Estação",
+                        "4300-173",
+                        "Porto"
+                    );
+
+INSERT INTO Estacao (   
+                        nome,
+                        morada,
+                        codigoPostal,
+                        localidade
+                    )
+                    VALUES(
+                        "São Bento",
+                        "Praça Almeida Garrett",
+                        "4000-069",
+                        "Porto"
+                    );
+
+INSERT INTO Estacao (   
+                        nome,
+                        morada,
+                        codigoPostal,
+                        localidade
+                    )
+                    VALUES(
+                        "Santa Apolónia",
+                        "Av. Infante D. Henrique",
+                        "1149-066",
+                        "Lisboa"
+                    );
+
+INSERT INTO Estacao (   
+                        nome,
+                        morada,
+                        codigoPostal,
+                        localidade
+                    )
+                    VALUES(
+                        "Oriente",
+                        "Edifício Gare do Oriente, Piso 1 - Av. D. João II",
+                        "4000-069",
+                        "Lisboa"
+                    );
+
+INSERT INTO Estacao (   
+                        nome,
+                        morada,
+                        codigoPostal,
+                        localidade
+                    )
+                    VALUES(
+                        "Coimbra",
+                        "Largo da Ameias",
+                        "3000-024",
+                        "Coimbra"
+                    );
+
+INSERT INTO Estacao (   
+                        nome,
+                        morada,
+                        codigoPostal,
+                        localidade
+                    )
+                    VALUES(
+                        "Coimbra-B",
+                        "Rua do Padrão - Eiras",
+                        "3025-045",
+                        "Coimbra"
+                    );
+
+INSERT INTO Estacao (   
+                        nome,
+                        morada,
+                        codigoPostal,
+                        localidade
+                    )
+                    VALUES(
+                        "Figueira da Foz",
+                        "Largo da Estação",
+                        "3080-021",
+                        "Figueira da Foz"
+                    );
+
+INSERT INTO Estacao (   
+                        nome,
+                        morada,
+                        codigoPostal,
+                        localidade
+                    )
+                    VALUES(
+                        "Aveiro",
+                        "Largo da Estação dos Caminhos de Ferro",
+                        "3800-179",
+                        "Aveiro"
+                    );
+
+INSERT INTO Estacao (   
+                        nome,
+                        morada,
+                        codigoPostal,
+                        localidade
+                    )
+                    VALUES(
+                        "Pampilhosa",
+                        "Rua da Estação de Caminhos de Ferro",
+                        "3050-443",
+                        "Pampilhosa"
+                    );
+
+INSERT INTO Estacao (   
+                        nome,
+                        morada,
+                        codigoPostal,
+                        localidade
+                    )
+                    VALUES(
+                        "Oliveira do Bairro",
+                        "Largo da Estação",
+                        "3770",
+                        "Oliveira do Bairro"
+                    );
