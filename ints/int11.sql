57568fd0dc61572aae3518e8c3e71fdf6a783558
rodrigotuna <rodrigotuna.andrade@gmail.com>
2021-05-18 16:00:45 +0100

    int11 update

--- ints/int11.sql
+++ ints/int11.sql
@@ -2,20 +2,14 @@
 .headers	on
 .nullvalue	NULL
 
-
--- SELECT distinct nome, idViagem, nomeEstacaoPartida as nomeEstacao, nomeEstacaoChegada
--- FROM Cliente JOIN Bilhete USING(nifCliente);
-
--- SELECT  nifCliente , nomeEstacaoPartida,  nomeEstacaoChegada, nomeEstacao, idViagem, tempoDeChegada  
--- FROM Bilhete JOIN INFORMACAO
--- WHERE nomeEstacao = nomeEstacaoPartida OR nomeEstacao = nomeEstacaoChegada;
-
-SELECT nome, duracaoMaxima
+SELECT nome, max(chegada - partida) as duracaoMaxima
 FROM Cliente JOIN(
-    SELECT nifCliente, max(tempoDeChegada) as duracaoMaxima
-    FROM(
-        SELECT  *
-        FROM Bilhete JOIN Viagem USING(idViagem) JOIN Informacao USING(idRota)
-        WHERE nomeEstacao = nomeEstacaoChegada
-    )   GROUP BY nifCliente
-) USING(nifCliente);
+    SELECT nifCliente, tempoDeChegada as chegada
+    FROM Cliente JOIN Bilhete USING(nifCliente) JOIN Viagem USING(idViagem) JOIN Rota USING(idRota) JOIN Informacao USING (idRota)
+    WHERE nomeEstacaoChegada = nomeEstacao
+) USING (nifCliente)
+JOIN (
+    SELECT nifCliente, tempoDeChegada as partida
+    FROM Cliente JOIN Bilhete USING(nifCliente) JOIN Viagem USING(idViagem) JOIN Rota USING(idRota) JOIN Informacao USING (idRota)
+    WHERE nomeEstacaoPartida = nomeEstacao
+) USING (nifCliente) GROUP BY nifCliente;
