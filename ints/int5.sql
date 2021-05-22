bed9afa9ba08b9dc9881586e94588943dd35ada6
Joao Andrade <up201905589@fe.up.pt>
2021-05-22 19:07:30 +0100

    Changed int5.sql

--- ints/int5.sql
+++ ints/int5.sql
@@ -6,7 +6,7 @@ SELECT nomeEstacao, COUNT(idBilhete) AS visits
 FROM(
     SELECT *
     FROM Estacao JOIN Informacao USING(nomeEstacao) JOIN Viagem USING(idRota) JOIN Bilhete USING(idViagem) 
-    WHERE strftime('%Y-%m-%d', dataDePartida) = '2021-05-10' OR strftime('%Y-%m-%d', dataDeChegada) = '2021-05-10'
+    WHERE strftime('%Y-%m', dataDePartida) = '2021-05' OR strftime('%Y-%m', dataDeChegada) = '2021-05'
     ) GROUP BY(nomeEstacao)
 ORDER BY visits DESC LIMIT 5;
  
